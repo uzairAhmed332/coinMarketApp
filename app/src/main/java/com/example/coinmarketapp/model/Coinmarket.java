6937ec8f38202d878ddc058173eaeda5a2f29ff1
Uzair <uzair.ahmed@nayapay.com>
2019-07-12 17:11:34 +0500

    models

---
+++ app/src/main/java/com/example/coinmarketapp/model/Coinmarket.java
@@ -0,0 +1,33 @@
+
+package com.example.coinmarketapp.model;
+
+import java.util.List;
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class Coinmarket {
+
+    @SerializedName("status")
+    @Expose
+    private Status status;
+    @SerializedName("data")
+    @Expose
+    private List<Datum> data = null;
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+
+    public List<Datum> getData() {
+        return data;
+    }
+
+    public void setData(List<Datum> data) {
+        this.data = data;
+    }
+
+}
---
+++ app/src/main/java/com/example/coinmarketapp/model/Quote.java
@@ -0,0 +1,21 @@
+
+package com.example.coinmarketapp.model;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class Quote {
+
+    @SerializedName("USD")
+    @Expose
+    private USD uSD;
+
+    public USD getUSD() {
+        return uSD;
+    }
+
+    public void setUSD(USD uSD) {
+        this.uSD = uSD;
+    }
+
+}
